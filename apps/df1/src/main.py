a98f95d767f34c76bcbba7c4b3a9d1320d1176ca
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-25 13:44:03 +0000

    feat(apps/df1): Create producer

---
+++ apps/df1/src/main.py
@@ -0,0 +1,35 @@
+from confluent_kafka import Producer
+import time
+import os
+import json
+import random
+
+
+BROKER_ADDR = os.environ.get("BROKER_ADDR")
+TOPIC = os.environ.get("TOPIC")
+
+
+def main():
+    p = Producer({"bootstrap.servers": BROKER_ADDR})
+
+    i = 0
+    while True:
+        key = str(i)
+        msg = json.dumps({
+          "i": i,
+          "hostname": os.environ.get("HOSTNAME"),
+          "pet_name": random.choice(["Dela", "Nela"]),
+          "pet_kind": random.choice(["dog", "cat"]),
+        })
+        headers = {
+          "version": "v1",
+        }
+        p.produce(TOPIC, key=key, value=msg, headers=headers)
+        p.flush()
+        print(f"produced: key={key} msg={msg}")
+        i += 1
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
